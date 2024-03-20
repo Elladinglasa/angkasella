54e25af28a4c816183ac29cec5cd97e74f2a0618
Elladinglasa <156661268+Elladinglasa@users.noreply.github.com>
2024-03-20 18:15:42 +0800

    Add files via upload
    ily

---
+++ MainActivity.cs
@@ -0,0 +1,38 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using Android.Content;
+
+namespace main
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            // Find the ImageButton
+            ImageButton imageButton = FindViewById<ImageButton>(Resource.Id.simpleImageButton);
+
+            // Set click listener for the ImageButton
+             imageButton.Click += (  sender, e) => 
+            {
+                // Create an Intent to navigate to NextActivity
+                Intent intent = new Intent(this, typeof(ikaduha));
+                StartActivity(intent);
+            };
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
